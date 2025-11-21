98aa831925b5f7f73aa2caf9876d0dc81cb85fee
JoelMejia03 <127954408+JoelMejia03@users.noreply.github.com>
2025-11-20 22:58:28 -0500

    Update decryptor.sh

--- decryptor.sh
+++ decryptor.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-WORDLIST="500-worst-passwords.txt"
-ENC_FILE="mensaje-oculto"
-OUT_FILE="mensaje-decifrado"
-
-while IFS= read -r passwd; do
-    echo "Probando: $passwd"
-
-    openssl enc -d -aes256 -iter 1000 \
-        -in "$ENC_FILE" \
-        -out "$OUT_FILE" \
-        -pass pass:"$passwd" 2>/dev/null
-
-    if [ $? -eq 0 ]; then
-        echo ""
-        echo "===================================="
-        echo "[+] CONTRASEÑA ENCONTRADA: $passwd"
-        echo "[+] Archivo descifrado en $OUT_FILE"
-        echo "===================================="
-        exit 0
+for clave in $(cat diccionario.txt); do
+    if openssl enc -d -aes256 -iter 1000 \
+        -in mensaje-oculto \
+        -out mensaje-decifrado \
+        -pass pass:$clave 2>/dev/null; then
+        
+        echo "Clave encontrada: $clave"
+        break
     fi
+done
 
-done < "$WORDLIST"
 
-echo "No se encontró la contraseña en el diccionario."
