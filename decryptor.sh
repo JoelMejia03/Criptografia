440ec3013aaa6950a5b921aa8fb47790d4d57c23
JoelMejia03 <127954408+JoelMejia03@users.noreply.github.com>
2025-11-20 23:10:09 -0500

    Update decryptor.sh

--- decryptor.sh
+++ decryptor.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-for clave in $(cat diccionario.txt); do
+while IFS= read -r clave; do
 
-    # Intentar descifrar
     openssl enc -d -aes256 -iter 1000 \
         -in mensaje-oculto \
         -out mensaje-decifrado \
-        -pass pass:$clave 2>/dev/null
+        -pass pass:"$clave" 2>/dev/null
 
-    # Verificar si el resultado es TEXTO real
     if file mensaje-decifrado | grep -qi "text"; then
-        echo "Clave REAL encontrada: $clave"
+        echo "Clave REAL encontrada: '$clave'"
         exit 0
     fi
 
-    # Si no es texto, eliminarlo
     rm -f mensaje-decifrado
 
-done
+done < diccionario.txt
 
 echo "No se encontró ninguna clave válida."
