1a2d92b6d2dd98ad4f2e1f9d3f7ca90d026e7210
JoelMejia03 <127954408+JoelMejia03@users.noreply.github.com>
2025-11-20 20:13:38 -0500

    Update decryptor.sh

--- decryptor.sh
+++ decryptor.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 for passwd in $(500-worst-passwords.txt); do
-	openssl enc -d -aes256 -iter 1000 -k "$passwd" -in mensaje.oculto -out mensaje.decifrado 2>/dev/null
+	openssl enc -d -aes256 -iter 1000 -k "$passwd" -in mensaje-oculto -out mensaje.decifrado 2>/dev/null
 
 if [$? -eq 0]; then
 	echo -e "[+] La password correcta es: $passwd"
 	break
 fi
 done
+
